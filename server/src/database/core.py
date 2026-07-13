0e58da72c5635e1f6ea90da1acbd217b606d7679
harshavardhan-git7 <harsha71.b@gmail.com>
2026-07-13 17:38:11 +0530

    Added database schema entities

--- server/src/database/core.py
+++ server/src/database/core.py
@@ -1,16 +1,22 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.orm import declarative_base, sessionmaker
 import os
 
 SQLALCHEMY_DATABASE_URL = os.getenv(
-    "DATABASE_URL", 
+    "DATABASE_URL",
     "postgresql://postgres:admin123@localhost:5432/contractiq"
 )
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+SessionLocal = sessionmaker(
+    autocommit=False,
+    autoflush=False,
+    bind=engine
+)
 
 Base = declarative_base()
+
 
 def get_db():
     db = SessionLocal()
