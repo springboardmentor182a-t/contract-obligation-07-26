13af685b8ffac742ffe0eb17f2f335cacb4f70d0
Rashmitha <rashmithamadupu28@gmail.com>
2026-07-16 17:18:33 +0530

    Migrate frontend from Vite to Create React App

--- server/src/main.py
+++ server/src/main.py
@@ -12,7 +12,6 @@ app = FastAPI(
 app.add_middleware(
     CORSMiddleware,
     allow_origins=[
-        "http://localhost:5173",
         "http://localhost:3000",
     ],
     allow_credentials=True,
