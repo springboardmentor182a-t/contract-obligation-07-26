2d81c0d82067a778201262264c927fa3717ed7d9
anjali160320 <anjalijandhyala16@gmail.com>
2026-07-27 21:01:29 +0530

    Use configurable API base URL

--- server/src/main.py
+++ server/src/main.py
@@ -2,7 +2,7 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
 from src.api import api_router
-from src.logging import configure_logging
+from src.logger_config import configure_logging
 from src.rate_limiter import init_rate_limiter
 
 configure_logging()
