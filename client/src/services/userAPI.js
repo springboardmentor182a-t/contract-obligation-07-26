95515b767573ec5565dea4f2ceac060e2c252b37
varshinigoud044 <varshinigoud044@gmail.com>
2026-08-03 22:00:47 +0530

    Fix User Management API endpoint

--- client/src/services/userAPI.js
+++ client/src/services/userAPI.js
@@ -1,17 +1,19 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "/api",
+  baseURL: "https://contract-obligation-demo-group-c.onrender.com/api",
 });
 
 export const getUsers = async () => {
   const response = await API.get("/users");
   return response.data;
 };
+
 export const updateUser = async (id, userData) => {
   const response = await API.put(`/users/${id}`, userData);
   return response.data;
 };
+
 export const deleteUser = async (id) => {
   const response = await API.delete(`/users/${id}`);
   return response.data;
