e4eee632573d6bdbe6f427f0835aa53ae2bb6f05
deepach0722 <deepach0722@gmail.com>
2026-08-05 12:50:22 +0530

    connect deployed frontend to backend

--- client/src/index.js
+++ client/src/index.js
@@ -4,6 +4,24 @@ import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
+const API_BASE_URL =
+  process.env.REACT_APP_API_URL ||
+  "http://127.0.0.1:8000/api";
+
+const originalFetch = window.fetch.bind(window);
+
+window.fetch = (input, options) => {
+  if (typeof input === "string" && input.startsWith("/api")) {
+    const apiPath = input.substring(4);
+
+    return originalFetch(
+      `${API_BASE_URL}${apiPath}`,
+      options
+    );
+  }
+
+  return originalFetch(input, options);
+};
 root.render(
   <React.StrictMode>
     <App />
